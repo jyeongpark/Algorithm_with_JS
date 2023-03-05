368a3de47535c761a09f06262c9ce0883de22d82
JIYEONGSTAR <jiyeongstar@gmail.com>
2023-03-05 20:16:35 +0900

    Edit Solve problem 20230305

--- p개인정보 수집 유효기간.js
+++ p개인정보 수집 유효기간.js
@@ -1,36 +1,53 @@
 const solution = (today, terms, privacies) => {
   let answer = [];
+  let [year, month, date] = today.split(".").map(Number); //숫자로 변경하여 변수에 할당
+  let todayNumber = year * 12 * 28 + month * 28 + date;
+
   let termObj = {};
-  terms.map((el) => {
+  terms.forEach((el) => {
     let [alpha, month] = el.split(" ");
     termObj[alpha] = Number(month);
-  });
-  let expire = privacies.map((el) => {
-    let [dates, a] = el.split(" ");
-    let [year, month, date] = dates.split(".").map((el) => Number(el));
-    date = date - 1 === 0 ? 28 : date - 1;
-    month = date === 28 ? month + termObj[a] - 1 : month + termObj[a];
-    if (month > 12) {
-      year +=
-        month % 12 === 0 ? parseInt(month / 12) - 1 : parseInt(month / 12);
-      month = month % 12 === 0 ? 12 : month % 12;
-    }
-    return `${year}-${month}-${date}`;
-  });
-  expire.map((el, index) => {
-    let [eY, eM, eD] = el.split("-").map((e) => Number(e));
-    let [tY, tM, tD] = today.split(".").map((el) => Number(el));
-    console.log(eY, eM, eD, tY, tM, tD);
-    if (eY < tY) {
-      answer.push(index + 1);
-    } else if (eY === tY && eM < tM) {
-      answer.push(index + 1);
-    } else if (eY === tY && eM === tM && eD < tD) {
-      answer.push(index + 1);
-    }
+  }); // 기간을 객체로
+
+  privacies.forEach((el, idx) => {
+    let [day, t] = el.split(" ");
+    day = day.split(".").map(Number);
+    let privacyNumber =
+      day[0] * 12 * 28 + day[1] * 28 + day[2] + termObj[t] * 28; //날짜에 term 추가
+    if (privacyNumber <= todayNumber) answer.push(idx + 1);
   });
   return answer;
 };
+
+// const solution = (today, terms, privacies) => {
+//   let answer = [];
+
+//   let expire = privacies.map((el) => {
+//     let [dates, a] = el.split(" ");
+//     let [year, month, date] = dates.split(".").map((el) => Number(el));
+//     date = date - 1 === 0 ? 28 : date - 1;
+//     month = date === 28 ? month + termObj[a] - 1 : month + termObj[a];
+//     if (month > 12) {
+//       year +=
+//         month % 12 === 0 ? parseInt(month / 12) - 1 : parseInt(month / 12);
+//       month = month % 12 === 0 ? 12 : month % 12;
+//     }
+//     return `${year}-${month}-${date}`;
+//   });
+//   expire.map((el, index) => {
+//     let [eY, eM, eD] = el.split("-").map((e) => Number(e));
+//     let [tY, tM, tD] = today.split(".").map((el) => Number(el));
+
+//     if (eY < tY) {
+//       answer.push(index + 1);
+//     } else if (eY === tY && eM < tM) {
+//       answer.push(index + 1);
+//     } else if (eY === tY && eM === tM && eD < tD) {
+//       answer.push(index + 1);
+//     }
+//   });
+//   return answer;
+// };
 
 console.log(
   solution(
